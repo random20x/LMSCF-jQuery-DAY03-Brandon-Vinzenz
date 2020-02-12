370b52561c9907a8cd44c40be4aaf77e47a0c23d
Brandon Landrum-Jeffries <Brandonlj8@gmail.com>
2020-02-12 14:13:48 +0100

    nlksdnlkdslk

---
+++ intermediate/javascript.js
@@ -0,0 +1,6 @@
+$(".container > .col-xs-12:nth-child(1) .product-image").css("background-color", "red");
+$(".container > .col-xs-12:nth-child(2) .product-image").css("background-color", "red");
+$(".container > .col-xs-12:nth-child(3) .product-image").css("background-color", "blue");
+$(".container > .col-xs-12:nth-child(4) .product-image").css("background-color", "blue");
+$(".container > .col-xs-12:nth-child(5) .product-image").css("background-color", "green");
+$(".container > .col-xs-12:nth-child(6) .product-image").css("background-color", "green");

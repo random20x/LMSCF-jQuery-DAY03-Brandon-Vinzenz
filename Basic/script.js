21bf6853036b92d2491e54884e7557b2d068c1dd
Vinzenz Sauer <vinzenz.sauer@gmx.at>
2020-02-12 11:08:04 +0100

    basic_bonus

--- Basic/script.js
+++ Basic/script.js
@@ -1,16 +1,21 @@
 $(document).ready(function(){
+    
+
+    $('#btn_createList').click(function(){
+        $('.ul_current').append($('<li>', {
+                text: $('#input_listItem').val()
+        }));
+    });  
     $('#animate').fadeIn(5000).delay(2000).slideUp(5000);
+    $('li').on('click', function() {
+        $(this).animate({
+                opacity: 0.0,
+                paddingLeft: '+=80'
+            }, 500, function() {
+                $(this).remove();
+            });
+        });
 });
 
-$(document).ready(function() {
-    $('li').on('click', function() {
-            $(this).animate({
-                    opacity: 0.0,
-                    paddingLeft: '+=80'
-                }, 500, function() {
-                    $(this).remove();
-                });
-            });
-    });
 
-    
+

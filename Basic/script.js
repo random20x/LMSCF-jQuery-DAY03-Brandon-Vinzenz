7b2e1c8b16aa2732f9b99572d61f2245510d6290
Vinzenz Sauer <vinzenz.sauer@gmx.at>
2020-02-12 10:43:56 +0100

    basic_finish

--- Basic/script.js
+++ Basic/script.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
     $('#animate').fadeIn(5000).delay(2000).slideUp(5000);
-    $('li').on('click', function(){  
-        $(this).animate(2000){
-            opacity:0.0;
-            paddingLeft: '+=80'
-        }, 500, function() {
-            $(this).remove();
-        });
-    });
 });
 
+$(document).ready(function() {
+    $('li').on('click', function() {
+            $(this).animate({
+                    opacity: 0.0,
+                    paddingLeft: '+=80'
+                }, 500, function() {
+                    $(this).remove();
+                });
+            });
+    });
+
+    
